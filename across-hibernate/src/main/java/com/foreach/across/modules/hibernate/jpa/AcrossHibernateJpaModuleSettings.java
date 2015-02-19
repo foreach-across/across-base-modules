032ec67c7bb6a67bd80eaa155935b5b096977f58
Nick Verhelst <nick.verhelst@mediafin.be>
2015-02-19 11:29:39 +0100

    jpa dialect property

--- across-hibernate/src/main/java/com/foreach/across/modules/hibernate/jpa/AcrossHibernateJpaModuleSettings.java
+++ across-hibernate/src/main/java/com/foreach/across/modules/hibernate/jpa/AcrossHibernateJpaModuleSettings.java
@@ -1,7 +1,23 @@
 package com.foreach.across.modules.hibernate.jpa;
 
+import com.foreach.across.core.AcrossModuleSettingsRegistry;
 import com.foreach.across.modules.hibernate.AcrossHibernateModuleSettings;
+import org.springframework.orm.jpa.JpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 
 public class AcrossHibernateJpaModuleSettings extends AcrossHibernateModuleSettings
 {
+
+	public static final String HIBERNATE_JPA_DIALECT = "acrossHibernateJpa.dialect";
+
+	@Override
+	protected void registerSettings( AcrossModuleSettingsRegistry registry ) {
+		super.registerSettings( registry );
+		registry.register( HIBERNATE_JPA_DIALECT, JpaDialect.class, new HibernateJpaDialect(),
+		                   "Specify the JpaDialect. Allows for configuring custom implementations." );
+	}
+
+	public JpaDialect getJpaDialect(){
+		return getProperty( HIBERNATE_JPA_DIALECT, JpaDialect.class );
+	}
 }
