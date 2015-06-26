240fa2a070f602b64ff70b92ff4cec4eadeaef4d
Arne Vandamme <arne@foreach.be>
2015-06-26 11:49:21 +0200

    Make naming consistent

--- src/main/java/com/foreach/across/modules/hibernate/config/PersistenceContextInViewConfiguration.java
+++ src/main/java/com/foreach/across/modules/hibernate/config/PersistenceContextInViewConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.foreach.across.modules.hibernate.config;
 
 import com.foreach.across.core.AcrossModule;
@@ -31,7 +46,7 @@ public class PersistenceContextInViewConfiguration
 	@Configuration
 	@AcrossDepends(required = "AcrossWebModule")
 	@AcrossCondition("settings.persistenceContextInView.toString() == 'FILTER'")
-	public static class OpenEntityManagerInViewFilterConfiguration extends AcrossWebDynamicServletConfigurer
+	public static class OpenSessionFactoryInViewFilterConfiguration extends AcrossWebDynamicServletConfigurer
 	{
 		@Autowired
 		private SessionFactory sessionFactory;
@@ -77,7 +92,7 @@ public class PersistenceContextInViewConfiguration
 	@Configuration
 	@AcrossDepends(required = "AcrossWebModule")
 	@AcrossCondition("settings.persistenceContextInView.toString() == 'INTERCEPTOR'")
-	public static class OpenEntityManagerInViewInterceptorConfiguration extends WebMvcConfigurerAdapter implements Ordered
+	public static class OpenSessionFactoryInViewInterceptorConfiguration extends WebMvcConfigurerAdapter implements Ordered
 	{
 		@Autowired
 		private SessionFactory sessionFactory;
