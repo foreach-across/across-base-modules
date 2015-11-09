260f92a38719da4156eb6ce6178d3b4e38bffdf5
Marc Vanbrabant <marc@foreach.be>
2015-11-09 10:43:21 +0100

    AXDWM-13 allow true debugging of the underlying filterMaps

--- debug-web-module/src/main/java/com/foreach/across/modules/debugweb/controllers/ServletContextInfoController.java
+++ debug-web-module/src/main/java/com/foreach/across/modules/debugweb/controllers/ServletContextInfoController.java
@@ -8,11 +8,15 @@ import com.foreach.across.modules.web.table.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.ui.Model;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 @DebugWebController
@@ -34,19 +38,76 @@ public class ServletContextInfoController
 	public String showFilters( Model model ) {
 		Map<String, ? extends FilterRegistration> filters = servletContext.getFilterRegistrations();
 
-		Table table = new Table( "Servlet filters" );
+		Table table = new Table( "Registered servlet filters" );
 
-		int index = 0;
 		for ( Map.Entry<String, ? extends FilterRegistration> entry : filters.entrySet() ) {
 			FilterRegistration filter = entry.getValue();
 
-			table.addRow( ++index, entry.getKey(), filter.getClassName() );
+			table.addRow( entry.getKey(), filter.getClassName() );
 		}
 
 		model.addAttribute( "filtersTable", table );
 
+        Table filterMapsTable = new Table("Order of registered servlet filters");
+        List<String> filterMaps = getFilterMaps();
+        if (filterMaps != null) {
+            model.addAttribute("filterMaps", filterMapsTable);
+            int index = 0;
+            for (String filterMap : filterMaps) {
+                filterMapsTable.addRow(index++, filterMap);
+            }
+        }
+
 		return DebugWeb.VIEW_SERVLET_FILTERS;
 	}
+
+    public List<String> getFilterMaps() {
+        Object applicationContext = getField(servletContext, "context");
+        if (applicationContext != null) {
+            Object standardContext = getField(applicationContext, "context");
+            if (servletContext != null) {
+                Object filterMapsObject = getField(standardContext, "filterMaps");
+                if( filterMapsObject != null ) {
+                    Object filterMapArray = getField(filterMapsObject, "array");
+                    if ( filterMapArray == null) {
+                        // Jboss holds a FilterMap[] inside the StandardContext
+                        return getFieldMapList( filterMapsObject );
+                    } else {
+                        // Tomcat uses an internal object to represent the filter maps
+                        return getFieldMapList( filterMapArray );
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private List<String> getFieldMapList(Object filterMapArray) {
+        if( filterMapArray.getClass().isArray() ) {
+            List<String> filterMaps;
+            filterMaps = new LinkedList<>();
+            for (Object item : (Object[] )filterMapArray) {
+                filterMaps.add(item.toString());
+            }
+            return filterMaps;
+        } else {
+            return null;
+        }
+    }
+
+    public Object getField(Object object, String fieldName) {
+        Field f = ReflectionUtils.findField(object.getClass(), fieldName);
+        if (f != null) {
+            try {
+                ReflectionUtils.makeAccessible(f);
+                return ReflectionUtils.getField(f, object);
+            } catch ( Exception e) {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
 
 	@RequestMapping("/servlet/servlets")
 	public String showServlets( Model model ) {
