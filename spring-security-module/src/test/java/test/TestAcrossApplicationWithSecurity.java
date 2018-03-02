fffcd9a89b3e727e665afe388c37c1f8188d2115
Arne Vandamme <arne@foreach.be>
2018-03-02 08:22:43 +0100

    Add test for global AuthenticationManagerBuilder configuration

--- spring-security-module/src/test/java/test/TestAcrossApplicationWithSecurity.java
+++ spring-security-module/src/test/java/test/TestAcrossApplicationWithSecurity.java
@@ -43,7 +43,7 @@ import static org.junit.Assert.assertNotNull;
 @DirtiesContext
 @WebAppConfiguration
 @ContextConfiguration(classes = SpringSecurityTestApplication.class)
-public class TestSpringSecurityWithTomcat
+public class TestAcrossApplicationWithSecurity
 {
 	@Autowired
 	private AcrossContextBeanRegistry contextBeanRegistry;
