bcf3a0a06cabbc07fa7ae7d7a46c917558830de2
Andy Somers <asm@foreach.be>
2018-06-26 15:01:06 +0200

    added appender for methods log

---
+++ logging-module/src/main/java/com/foreach/across/modules/logging/method/MethodLoggerJsonProvider.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.foreach.across.modules.logging.method;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.fasterxml.jackson.core.JsonGenerator;
+import net.logstash.logback.composite.AbstractFieldJsonProvider;
+import net.logstash.logback.composite.JsonWritingUtils;
+
+import java.io.IOException;
+
+public class MethodLoggerJsonProvider extends AbstractFieldJsonProvider<ILoggingEvent>
+{
+	@Override
+	public void writeTo( JsonGenerator generator, ILoggingEvent event ) throws IOException {
+		String message = event.getFormattedMessage();
+		String[] split = message.split( "\t" );
+		int methodLevel = Integer.parseInt( split[0] );
+		String method = split[1];
+		int duration = Integer.parseInt( split[2] );
+		JsonWritingUtils.writeNumberField( generator, "methodLevel", methodLevel );
+		JsonWritingUtils.writeStringField( generator, "method", method );
+		JsonWritingUtils.writeNumberField( generator, "duration", duration );
+	}
+}
