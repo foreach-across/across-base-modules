948a49f3930c9ac5f64890e3eea6b84060ad37fa
Marc Vanbrabant <marc@foreach.be>
2019-09-04 09:53:09 +0200

    Release 3.0.0-SNAPSHOT (AXP 5)

--- logging-module/src/test/java/com/foreach/across/modules/logging/exception/TestExceptionToMailResolver.java
+++ logging-module/src/test/java/com/foreach/across/modules/logging/exception/TestExceptionToMailResolver.java
@@ -20,7 +20,7 @@ import com.foreach.common.spring.context.ApplicationEnvironment;
 import com.foreach.common.spring.mail.MailService;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Matchers;
+import org.mockito.ArgumentMatchers;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockHttpSession;
@@ -31,8 +31,8 @@ import java.io.File;
 import java.util.Date;
 import java.util.Map;
 
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 public class TestExceptionToMailResolver
@@ -73,8 +73,8 @@ public class TestExceptionToMailResolver
 
 		resolver.doResolveException( request, response, null, new Exception() );
 
-		verify( mailService ).sendMimeMail( eq( fromAddress ), eq( toAddress ), anyString(), anyString(), anyString(),
-		                                    Matchers.<Map<String, File>>anyObject() );
+		verify( mailService ).sendMimeMail( eq( fromAddress ), eq( toAddress ), isNull(), anyString(), anyString(),
+		                                    isNull() );
 	}
 
 	@Test
@@ -83,7 +83,7 @@ public class TestExceptionToMailResolver
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
 		when( mailService.sendMimeMail( anyString(), anyString(), anyString(), anyString(), anyString(),
-		                                Matchers.<Map<String, File>>anyObject() ) ).thenThrow(
+		                                ArgumentMatchers.<Map<String, File>>any() ) ).thenThrow(
 				new NullPointerException() );
 
 		resolver.doResolveException( request, response, null, new Exception() );
@@ -116,8 +116,7 @@ public class TestExceptionToMailResolver
 
 		resolver.doResolveException( request, response, null, new Exception() );
 
-		verify( mailService ).sendMimeMail( eq( fromAddress ), eq( toAddress ), anyString(), anyString(), anyString(),
-		                                    Matchers.<Map<String, File>>anyObject() );
+		verify( mailService ).sendMimeMail( eq( fromAddress ), eq( toAddress ), isNull(), anyString(), anyString(), isNull() );
 	}
 
 }
